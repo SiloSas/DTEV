e005265a7417162f03df1e1b022e9a56629ee7a2
SiloSas <loann@debian>
2015-12-11 13:51:57 +0100

    add rooms and services

--- server/conf/evolutions/default/1.sql
+++ server/conf/evolutions/default/1.sql
@@ -10,9 +10,20 @@ CREATE TABLE rooms (
 );
 
 INSERT INTO rooms(id, name, presentation, header, images, isAnApartment, price) VALUES
-  ('a4aea509-1002-47d0-b55c-593c91cb32ae', 'dsdlksd', 'kdskdjsk', 'kdskdjsk', 'assets/images/desToits1.jpg', false, '5€');
+  ('a4aea509-1002-47d0-b55c-593c91cb32ae', 'Passe Simple', 'La chambre, au premier étage de notre maison d’hôtes, calme et spacieuse, bénéficie d’un accès indépendant. Avec lit double en 160 cm, salle de bain avec baignoire et WC privatif indépendant, coin collation, bouilloire et cafetière électrique.',
+   'Vous apprécierez les moments de partage autour d’un généreux petit-déjeuner sur la terrasse ou dans notre salle à manger.',
+    'assets/images/passesimple.jpg', false, '65 € / Nuit');
 INSERT INTO rooms(id, name, presentation, header, images, isAnApartment, price) VALUES
-  ('a4aea509-1002-47d0-b55c-593c91cb38ae', 'dsdlksda', 'kdskdjsks', 'kdskdjskc', 'assets/images/doubleBed.jpg', false, '5€');
+  ('a4aea509-1002-47d0-b5c-593c91cb38ae', 'Pied à Terre', 'Maecenas id mattis ipsum. Quisque dictum dolor dolor, a tincidunt nisl tincidunt id. Pellentesque',
+  'Maecenas id mattis ipsum. Quisque dictum dolor dolor, a tincidunt nisl tincidunt id. Pellentesque', 'assets/images/doubleBed.jpg', true, ' 65 € / Nuit');
+INSERT INTO rooms(id, name, presentation, header, images, isAnApartment, price) VALUES
+  ('a4aea509-1002-47d0-b55c-593c91b38ae', 'L’Escale', 'Maecenas id mattis ipsum. Quisque dictum dolor dolor, a tincidunt nisl tincidunt id. Pellentesque',
+  'Vous apprécierez les moments de partage autour d’un généreux petit-déjeuner sur la terrasse ou dans notre salle à manger.',
+  'assets/images/blueBed2.jpg', true, ' 65 € / Nuit');
+INSERT INTO rooms(id, name, presentation, header, images, isAnApartment, price) VALUES
+  ('a4aea509-1002-47d0-b55c-593c91cb3ae', 'La terrasse', 'Appartement T1bis au 2ème étage d une maison d hôtes comprenant 2 autres appartements et une chambre d hôtes, avec salon et couchage en 140, petite alcove pour accueillir une 3ème personne ou un enfant. Grande terrasse "sur le toit" avec salon de jardin. Cuisine équipée indépendante. Parking en supplément.',
+  'Ce lumineux appartement de 35 m² composé d une chambre spacieuse et confortable vous séduira par sa tranquillité ',
+  'assets/images/blueBed.jpg', true, ' 65 € / Nuit - 364 € / Semaine');
 
 
 CREATE TABLE agreements (
@@ -24,13 +35,13 @@ CREATE TABLE agreements (
 );
 
 INSERT INTO agreements(id, title, description, image) VALUES
-  ('a4aea509-1002-47d0-b55c-593c91cb32ae', 'dsdlksd', 'dsdlksdadss', 'assets/images/desToits1.jpg');
+  ('a4aea509-1002-47d0-b55c-593c91cb32ae', 'Salon de jardin', 'dsdlksdadss', 'assets/images/desToits1.jpg');
 INSERT INTO agreements(id, title, description, image) VALUES
-  ('a4aea509-1002-47d0-b55c-593c91cb38akjhhe', 'dsdlksda', 'dsdlksdads', 'assets/images/doubleBed.jpg');
+  ('a4aea509-1002-47d0-b55c-593c91b38akjhhe', 'Terrasse', 'dsdlksdads', 'assets/images/terrasse.jpg');
 INSERT INTO agreements(id, title, description, image) VALUES
-  ('a4aea509-1002-47d0d-b55c-593c91cb38akjhhe', 'dsdlksda', 'dsdlksdads', 'assets/images/doubleBed.jpg');
+  ('a4aea509-1002-47d0d-b55c-593c91cb38ajhhe', 'Parking', 'dsdlksdads', 'assets/images/parking.jpg');
 INSERT INTO agreements(id, title, description, image) VALUES
-  ('a4aea509-1002-47sd0-b55c-593c91cb38akjhhe', 'dsdlksda', 'dsdlksdads', 'assets/images/doubleBed.jpg');
+  ('a4aea509-1002-47sd0-b55c-593c91cb38akhhe', 'Plateau repas', 'dsdlksdads', 'assets/images/plateau-repas.jpg');
 
 
 CREATE TABLE comments (
@@ -44,11 +55,17 @@ CREATE TABLE comments (
 );
 
 INSERT INTO comments(id, title, comment, userName, rate, date) VALUES
-  ('a4aea509-1002-47d0-b55c-593c91cb32ae', 'dsdlksd', 'dsdlksdadss', 'fsfsdfsdf', 2, '2012-08-24 14:00:00' );
+  ('a4aea509-1002-47d0-b55c-593c91cb32ae', 'dsdlksd', 'dsdlksdadss', 'usfsdfsdf', 2, '2012-08-24 14:00:00' );
 INSERT INTO comments(id, title, comment, userName, rate, date) VALUES
   ('a4aea509-1002-47d0-b55c-593cd1cb32ae', 'dsdlksd', 'dsdlksdadss', 'fsfsdfsdf', 2, '2012-08-24 14:00:00' );
 INSERT INTO comments(id, title, comment, userName, rate, date) VALUES
-  ('a4aea509-1002-47d0-b55c-593cq1cb32ae', 'dsdlksd', 'dsdlksdadss', 'fsfsdfsdf', 2, '2012-08-24 14:00:00' );
+  ('a4aea509-1002-47d0-b55c-593cq1cb32ae', 'dsdlksd', 'dsdlksdadss', 'esfsdfsdf', 2, '2012-08-24 14:00:00' );
+INSERT INTO comments(id, title, comment, userName, rate, date) VALUES
+  ('a4aea509-1002-47d0-b55c-53cq1cb32ae', 'dsdlksd', 'dsdlksdadss', 'jsfsdfsdf', 2, '2012-08-24 14:00:00' );
+INSERT INTO comments(id, title, comment, userName, rate, date) VALUES
+  ('a4aea509-1002-47d0-b55c-53cq1cb32e', 'dsdlksd', 'dsdlksdadss', 'gsfsdfsdf', 2, '2012-08-24 14:00:00' );
+INSERT INTO comments(id, title, comment, userName, rate, date) VALUES
+  ('a4aea509-1002-4d0-b55c-53cqcb32e', 'dsdlksd', 'dsdlksdadss', 'bgsfsdfsdf', 2, '2012-08-24 14:00:00' );
 
 # --- !Downs
 
