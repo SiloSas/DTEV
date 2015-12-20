9ab494f421c47aea28a2a1720c4537e593400f82
Simon Garnier <simongarnier07@hotmail.fr>
2015-12-20 19:27:25 +0100

    Display admin page only if is connected

--- client/src/main/scala/BackEnd/BackEndController.scala
+++ client/src/main/scala/BackEnd/BackEndController.scala
@@ -1,17 +1,27 @@
 package BackEnd
 
-import com.greencatsoft.angularjs.core.RouteParams
+import com.greencatsoft.angularjs.core.{HttpService, RouteParams}
 import com.greencatsoft.angularjs.{AbstractController, injectable}
 import org.scalajs.dom.console
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js.annotation.JSExport
+
 
 @JSExport
 @injectable("backEndController")
-class BackEndController(scope: BackEndScope, $routeParams: RouteParams)
+class BackEndController(scope: BackEndScope, $routeParams: RouteParams, http: HttpService)
     extends AbstractController[BackEndScope](scope) {
 
-  scope.isConnected = false
+
+  val eventuallyIsConnected = http.get[String]("/isConnected")
+    .map(isConnected => isConnected.toBoolean)
+    .recover { case e: Exception => false}
+
+  eventuallyIsConnected map { isConnected =>
+    scope.isConnected = isConnected
+    scope.$digest()
+  }
 
   private def handleError(t: Throwable) {
     console.error(s"An error has occured: $t")
