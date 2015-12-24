eeccbbb247417b6d502c206e9c29323d1369028c
Simon Garnier <simongarnier07@hotmail.fr>
2015-12-24 03:02:42 +0100

    details

--- server/conf/evolutions/default/1.sql
+++ server/conf/evolutions/default/1.sql
@@ -17,22 +17,22 @@ INSERT INTO rooms(id, name, presentation, header, images, isAnApartment, price) 
   ('b5aea509-1002-47d0-b55c-593c91cb32ae', 'Lempicka',
    'La chambre, au premier étage de notre maison d’hôtes, calme et spacieuse, bénéficie d’un accès indépendant. Avec lit double en 160 cm, salle de bain avec baignoire et WC privatif indépendant, coin collation, bouilloire et cafetière électrique.',
    'Vous apprécierez les moments de partage autour d’un généreux petit-déjeuner sur la terrasse ou dans notre salle à manger.',
-    'assets/images/passesimple.jpg', false, '65 € / Nuit');
+    'assets/images/passesimple2.jpg', false, '65 € / Nuit');
 INSERT INTO rooms(id, name, presentation, header, images, isAnApartment, price) VALUES
   ('a4aea509-1002-47d0-b5c-593c91cb38ae', 'Pied à Terre',
    'Maecenas id mattis ipsum. Quisque dictum dolor dolor, a tincidunt nisl tincidunt id. Pellentesque',
   'Maecenas id mattis ipsum. Quisque dictum dolor dolor, a tincidunt nisl tincidunt id. Pellentesque',
-   'assets/images/doubleBed.jpg', true, '65 € / Nuit');
+   'assets/images/blueBed.jpg', true, '65 € / Nuit');
 INSERT INTO rooms(id, name, presentation, header, images, isAnApartment, price) VALUES
   ('a4aea509-1002-47d0-b55c-593c91b38ae', 'L’Escale',
    'Maecenas id mattis ipsum. Quisque dictum dolor dolor, a tincidunt nisl tincidunt id. Pellentesque',
   'Vous apprécierez les moments de partage autour d’un généreux petit-déjeuner sur la terrasse ou dans notre salle à manger.',
-  'assets/images/blueBed2.jpg', true, '65 € / Nuit');
+  'assets/images/IMG_5441.jpg', true, '65 € / Nuit');
 INSERT INTO rooms(id, name, presentation, header, images, isAnApartment, price) VALUES
   ('a4aea509-1002-47d0-b55c-593c91cb3ae', 'La terrasse',
    'Appartement T1bis au 2ème étage d une maison d hôtes comprenant 2 autres appartements et une chambre d hôtes, avec salon et couchage en 140, petite alcove pour accueillir une 3ème personne ou un enfant. Grande terrasse "sur le toit" avec salon de jardin. Cuisine équipée indépendante. Parking en supplément.',
   'Ce lumineux appartement de 35 m² composé d une chambre spacieuse et confortable vous séduira par sa tranquillité ',
-  'assets/images/blueBed.jpg', true, '65 € / Nuit - 364 € / Semaine');
+  'assets/images/IMG_5410.jpg', true, '65 € / Nuit - 364 € / Semaine');
 
 
 CREATE TABLE agreements (
@@ -45,7 +45,8 @@ INSERT INTO agreements(id, title, description, image) VALUES
   ('a4aea509-1002-47d0-b55c-593c91cb32ae', 'Jardin', 'Profitez d''un accès au salon de jardin',
    'assets/images/desToits1.jpg');
 INSERT INTO agreements(id, title, description, image) VALUES
-  ('a4aea509-1002-47d0-b55c-593c91b38akjhhe', 'Terrasse', 'petite description de la terrasse', 'assets/images/terrasse.jpg');
+  ('a4aea509-1002-47d0-b55c-593c91b38akjhhe', 'Terrasse', 'petite description de la terrasse',
+   'assets/images/terrasse.jpg');
 INSERT INTO agreements(id, title, description, image) VALUES
   ('a4aea509-1002-47d0d-b55c-593c91cb38ajhhe', 'Parking', 'Photo non contractuelle', 'assets/images/parking.jpg');
 INSERT INTO agreements(id, title, description, image) VALUES
@@ -88,8 +89,10 @@ CREATE TABLE reservations(
   departureDate             DATE
 );
 
+
 # --- !Downs
-DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS rooms CASCADE;
 DROP TABLE IF EXISTS agreements;
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reservations;
