af2b3bd0d10738896488bca5a1df1feb8486eedb
SiloSas <loann@debian>
2016-01-15 02:45:51 +0100

    margin, services, close cookies

--- client/src/main/scala/idea-shared-main/scala/shared/Agreement.scala
+++ client/src/main/scala/idea-shared-main/scala/shared/Agreement.scala
@@ -3,4 +3,4 @@ package shared
 import scala.scalajs.js.annotation.JSExportAll
 
 @JSExportAll
-case class Agreement(id: String, title: String, description: String, image: String)
+case class Agreement(id: String, var title: String, var description: String, var image: String)
